d4634f7c743aa49fe44632526c5ff302e38a1066
Olivier Halligon <olivier.halligon@automattic.com>
2025-11-28 18:49:16 +0100

    Introduce Buildkite pipeline

---
+++ .buildkite/build_and_upload_artifact.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "~~~ Building..."
+[ -f "${HOME}/.cargo/env" ] && source "${HOME}/.cargo/env"
+cargo build --release
+
+echo "~~~ Uploading artifact..."
+system=$(uname -s | tr '[:upper:]' '[:lower:]')
+arch=$(uname -m)
+version=$(git rev-parse --short HEAD)
+extension="${1:-}"
+target_name="a8c-git-secrets-${system}-${arch}-${version}${extension}"
+cp "target/release/a8c-git-secrets" "$target_name"
+buildkite-agent artifact upload "$target_name"
